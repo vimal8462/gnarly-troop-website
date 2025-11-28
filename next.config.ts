7d503a64072ad6e46acc9eeda578f8a8c1a91ec7
Bhanu Pratap Singh <iims.bhanu@gmail.com>
2025-11-28 18:33:05 +0530

    Change the team video issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: "export", // enables static export
+  trailingSlash: true
 };
 
 export default nextConfig;
